6d58342bcc33786e4e629f5814fafc2c1a1df6d8
velveteen-rever <velveteen.rever@gmail.com>
2024-04-03 19:28:37 -0600

    fix: missing import in _app. edit: about content, portfolio page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,18 @@ import "nextra-theme-blog/style.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import "../styles/main.css";
+import React from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link
-          rel="alternate"
-          type="application/rss+xml"
-          title="RSS"
-          href="/feed.xml"
-        />
+        {/* <link */}
+        {/*   rel="alternate" */}
+        {/*   type="application/rss+xml" */}
+        {/*   title="RSS" */}
+        {/*   href="/feed.xml" */}
+        {/* /> */}
         <link
           rel="preload"
           href="/fonts/Inter-roman.latin.var.woff2"
